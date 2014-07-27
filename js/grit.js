ef932908f4a253518b6499d8c120d530a76f484f
tmccarthy67 <deviowa@deviowas-MacBook-Air.local>
2014-07-26 21:52:56 -0500

    questions donegit add .git add .

--- js/grit.js
+++ js/grit.js
@@ -9,77 +9,21 @@ var nounPhrases = new Array;
 var GritData = new Array;
 var GritSumDataRaw = new Firebase('https://gritsum.firebaseio.com/phrases/');
 
-var startPhrases = [
-    // "First one to ", "Last one to ", "I will "
-    ];
+var startPhrases = [];
 
-var verbPhrases = [
-    // "eat ", "drink ", "run "
-    ];
+var verbPhrases = [];
 
 var quantPhrases = [];
 
-var nounPhrases = [
-    //     {
-    //     "verb": "eat ", // new property for this object
-    //     "noun": "burrito ",
-    //     "phrase": "2 Pancheros burritos "
-    //     },
+var nounPhrases = [];
 
-    // {
-    //     "verb": "eat ", // new property for this object
-    //     "noun": "pizza ",
-    //     "phrase": "a whole pizza "
-    //     },
+var rewardPhrases = [];
 
-    // {
-    //     "verb": "drink ", // new property for this object
-    //     "noun": "milk ",
-    //     "phrase": "a whole gallon of milk "
-    //     },
+var timePhrases = [];
 
-    // {
-    //     "verb": "drink ", // new property for this object
-    //     "noun": "shots ",
-    //     "phrase": "3 shots of Jack "
-    //     },
-    // {
-    //     "verb": "run ", // new property for this object
-    //     "noun": "distance ",
-    //     "phrase": "1 mile in under 7 minutes "
-    //     },
+var whoPhrases = [];
 
-    // {
-    //     "verb": "run ", // new property for this object
-    //     "noun": "speed ",
-    //     "phrase": "the beer mile "
-    //     }
-    ];
-
-var rewardPhrases = [
-    // "2 Panchero's Burritos from the loser " , "a free lunch from the loser ", "bragging rights"
-    ];
-
-var timePhrases = [
-    // "immediately ", " within 24 hours ", "within one week "
-    ];
-
-var whoPhrases = [
-    // {
-    //     "who": "challenger ",
-    //     "challengesStarted": "challenges started ",
-    //     "challengesCompleted": "challenges completed "
-    // },
-    // {
-    //     "who": "challengee ",
-    //     "challengesStarted": "challenges started ",
-    //     "challengesCompleted": "challenges completed "
-    // }
-    ];
-
-var verifyPhrases = [
-    //    "no", "yes"
-    ];
+var verifyPhrases = [];
 
 var GritRawPhrases = function (f) {
     if (startPhrases.length > 0) {
@@ -91,37 +35,17 @@ var GritRawPhrases = function (f) {
 
         startPhrases = GritData.startPhrases;
         verbPhrases = GritData.verbPhrases;
-        // nounPhrases = GritData.nounPhrases.franchise;
-        // console.log(nounPhrases);
         quantPhrases = GritData.nounPhrases.quantity;
         franchisePhrases = GritData.nounPhrases.franchise;
         rewardPhrases = GritData.rewardPhrases;
-        // console.log(franchisePhrases.drink);
-        // console.log(franchisePhrases.eat);
-        // console.log(franchisePhrases.physical);
+        verifyPhrases = GritData.verifyPhrases;
+        timePhrases = GritData.timePhrases;
         f();
-      // console.log(GritData.nounPhrases.franchise.drink);
-      // console.log(GritData.nounPhrases.franchise.eat);
-      // console.log(GritData.nounPhrases.franchise.eat.pancheros);
-      // console.log(GritData.nounPhrases.franchise.eat.pizzaHut);
-      // console.log(GritData.nounPhrases.franchise.eat.buffaloWildWings);
-      // console.log(GritData.nounPhrases.franchise.physical);
-      // console.log(GritData.nounPhrases.franchise.physical.bike);
-      // console.log(GritData.nounPhrases.franchise.physical.lose);
-      // console.log(GritData.nounPhrases.franchise.physical.run);
-      // console.log(GritData.rewardPhrases);
-      // console.log(GritData.rewardPhrases.loser);
-      // console.log(GritData.rewardPhrases.winner);
-      // console.log(GritData.timePhrases);
-      //return;
   });
 };
 
 var startGenerator = function () {
-// console.log(startPhrases);
-// console.log(startPhrases[0]);
 startConstructor(startPhrases);
-//return
   };
 
   var inputContract;
@@ -243,8 +167,6 @@ var quantConstructor = function() {
                 franchiseConstructor();
             });
         }
-// console.log(keyName);
-// console.log(quantPhrases);
         for (var keyName in quantPhrases) {
             attachHandlerQuant(keyName);
         }
@@ -259,15 +181,9 @@ var franchiseConstructor = function() {
 
         for (var keyName in franchisePhrases) {
 
-        // console.log(verb);
-        // console.log(franchisePhrases);
-        // console.log(franchisePhrases[verb]);
-        // console.log(keyName);
-        // console.log(verb === keyName);
             if (verb === keyName) {
                 for (var nounKey in franchisePhrases[verb]) {
                     var nounKeyClass = nounKey.replace(/\s/g, "");
-                    // console.log(nounKey);
             var franchiseClass = "franchise" + nounKeyClass;
             $('#franchiseBuild').append($('<p>').text(nounKey).addClass(franchiseClass))
                 }
@@ -289,15 +205,7 @@ var franchiseConstructor = function() {
                 nounConstructor();
             });
         }
-// console.log(verb);
-// console.log(keyName);
-// console.log(nounKey);
-// var test = nounKey;
-// var test2 = nounKey.trim();
-// console.log(test === test2);
-// var test = franchisePhrases[verb];
-// console.log(test);
-// console.log(test[nounKey]);
+
         for (var nounKey in franchisePhrases[verb]) {
             attachHandlerFranchise(nounKey);
         }
@@ -312,33 +220,15 @@ var nounConstructor = function() {
         var businessLocation = (business).trim();
         var output = buildContractFormat(newContractArray);
         var nouns = franchisePhrases[verb];
-        // console.log(nouns);
-        // console.log(business);
-        // console.log(businessLocation);
         var specificNouns = nouns[business];
-        // console.log(specificNouns);
         $('#nounBuild').append($('<p>').text(output).addClass('question'));
 
         for (var keyName in nouns) {
 
-        // console.log(keyName);
-        // console.log(businessLocation);
-        // console.log(business);
-        // console.log(franchisePhrases);
-        // console.log(verb);
-        // console.log(business === keyName);
             if (business === keyName) {
                 for (var nounKey in specificNouns) {
-        // console.log(nounKey);
-        // console.log(specificNouns);
-        // console.log(franchisePhrases[verb]);
-        // console.log(business);
-        // console.log(businessLocation);
                     var nounClass = "noun" + nounKey;
-                    // console.log(nounKeyClass);
-                    // console.log(nounKey);
-                    // console.log(specificNouns);
-                    // console.log(specificNouns[nounKey]);
+
             $('#nounBuild').append($('<p>').text(specificNouns[nounKey]).addClass(nounClass))
                 }
 
@@ -346,83 +236,65 @@ var nounConstructor = function() {
         }
 
         var attachHandlerNoun = function(nounKey) {
-            // console.log(nounKey);
             var nounClick = ".noun" + nounKey;
-            // console.log(nounClick);
-            // console.log(nounClick === nounClass);
             $(nounClick).click(function() {
-                // console.log(specificNouns);
                 tempNoun = specificNouns[nounKey];
-                // console.log(tempNoun);
                 newContractArray.push(tempNoun);
                 newContract = newContract + tempNoun;
-
-                // console.log(newContract);
-                // console.log(newContractArray);
 
                 $('#nounBuild').hide();
                 $('#rewardBuild').show();
                 rewardConstructor();
             });
         }
-// console.log(specificNouns);
-// console.log(specificNouns[nounKey]);
         for (var nounKey in specificNouns) {
-console.log(nounKey);
-console.log(specificNouns);
             attachHandlerNoun(nounKey);
         }
     });
 }
 
-var rewardConstructor = function(reward) {
-    console.log('done');
-    // var rewardText = newContractArray;
+var rewardConstructor = function() {
     var output = buildContractFormat(newContractArray);
     $('#rewardBuild').append($('<p>').text(output).addClass('question'))
+    for (var keyName in rewardPhrases.combined) {
+        var rewardClass = "reward" + keyName;
+        $('#rewardBuild').append($('<p>').text(rewardPhrases.combined[keyName]).addClass(rewardClass))
+    }
 
-    //  build in ability to have rewards match up with the type of challenge i.e. drinking challenges have drink rewards etc
-    for (i = 0; i < rewardPhrases.length; i++) {
-        var rewardClass = "reward" + i;
-    //     if (verb === rewardPhrases[i].reward) {
-        $('#rewardBuild').append($('<p>').text(rewardPhrases[i]).addClass(rewardClass))
-    //         }
-}
+        var attachHandlerReward = function(keyName) {
+            var rewardClick = ".reward" + keyName;
+            $(rewardClick).click(function () {
+                tempReward = rewardPhrases.combined[keyName];
+                newContractArray.push(tempReward);
+                newContract = newContract + tempReward;
 
-var attachHandlerNoun = function(i) {
-    var rewardClick = ".reward" + i;
-    $(rewardClick).click(function () {
-        tempReward = rewardPhrases[i];
-        newContractArray.push(tempReward);
-        newContract = newContract + tempReward;
+                $('#rewardBuild').hide();
+                $('#timeBuild').show();
+                timeConstructor();
 
-        $('#rewardBuild').hide();
-        $('#timeBuild').show();
-        timeConstructor();
+            });
+        }
 
-    });
-}
-
-for (i = 0; i < nounPhrases.length; i++) {
-    attachHandlerNoun(i);
-}
+    for (var keyName in rewardPhrases.combined) {
+        attachHandlerReward(keyName);
+    }
 
 }
 
 var timeConstructor = function() {
-    // var timeText = newContractArray;
+    console.log('done');
     var output = buildContractFormat(newContractArray);
     $('#timeBuild').append($('<p>').text(output).addClass('question'))
 
-    for (i = 0; i < timePhrases.length; i++) {
-        var timeClass = "time" + i;
-        $('#timeBuild').append($('<p>').text(timePhrases[i]).addClass(timeClass))
+    for (var keyName in timePhrases) {
+        var timeClass = "time" + keyName;
+        $('#timeBuild').append($('<p>').text(timePhrases[keyName]).addClass(timeClass))
     }
 
-    var attachHandlerTime = function(i) {
-        var timeClick = ".time" + i;
+    var attachHandlerTime = function(keyName) {
+        var timeClick = ".time" + keyName;
         $(timeClick).click(function() {
-            tempTime = timePhrases[i];
+            tempTime = timePhrases[keyName];
             newContractArray.push(tempTime);
             newContract = newContract + tempTime;
 
@@ -435,8 +307,8 @@ var timeConstructor = function() {
         });
     }
 
-    for (i = 0; i < startPhrases.length; i++) {
-        attachHandlerTime(i);
+    for (var keyName in timePhrases) {
+        attachHandlerTime(keyName);
     }
 
 }
@@ -457,16 +329,11 @@ var verifyConstructor = function() {
         var verifyClick = ".verify" + i;
         $(verifyClick).click(function() {
             tempVerify = verifyPhrases[i];
-            // newContractArray.push(tempTime);
-            // newContract = newContract + tempTime;
 
             $('#verifyBuild').hide();
+            $('#verification').hide();
             $('#sendBuild').show();
-            //            verifyConstructor();
-
-console.log(newContract);
-console.log(newContractArray);
-            // verifyConstructor();
+            sendConstructor();
 
         });
     }
@@ -474,6 +341,34 @@ console.log(newContractArray);
     for (i = 0; i < verifyPhrases.length; i++) {
         attachHandlerVerify(i);
     }
+
+}
+
+var sendConstructor = function() {
+
+    $('#sendBuild').append($('<p>').text("Sending to Facebook").addClass('question'));
+
+//     var output = buildContractFormat(newContractArray);
+//     $('#sendBuild').append($('<p>').text(output).addClass('question'))
+
+//     for (i = 0; i < sendPhrases.length; i++) {
+//         var sendClass = "send" + i;
+//         $('#sendBuild').append($('<p>').text(sendPhrases[i]).addClass(sendClass))
+//     }
+
+//     var attachHandlerSend = function(i) {
+//         var sendClick = ".send" + i;
+//         $(sendClick).click(function() {
+//             tempSend = sendPhrases[i];
+
+//             $('#sendBuild').hide();
+
+//         });
+//     }
+
+//     for (i = 0; i < sendPhrases.length; i++) {
+//         attachHandlerSend(i);
+//     }
 
 }
 
@@ -485,6 +380,3 @@ var buildContractFormat = function(CA) {
     }
     return output;
 }
-
-// console.log(who[0].who + "challenges " + who[1].who);
-// console.log(startPhrases[0] + nounPhrases[0].verb + nounPhrases[0].phrase + "wins " + reward + "to be paid " + timeFrame[0]);
